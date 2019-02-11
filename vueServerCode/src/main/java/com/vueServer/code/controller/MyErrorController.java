389be375ce33013ccdcdda5ef302c434d0cdb1db
jgma <1520458127@qq.com>
2019-02-11 15:49:27 +0800

    添加全局异常捕捉

--- vueServerCode/src/main/java/com/vueServer/code/controller/MyErrorController.java
+++ vueServerCode/src/main/java/com/vueServer/code/controller/MyErrorController.java
@@ -6,6 +6,9 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * @author MaJianGuo.
@@ -34,6 +37,12 @@ public class MyErrorController implements ErrorController {
     @Override
     public String getErrorPath() {
         log.error("出错了=======================");
-        return "Error";
+        return "/error";
+    }
+
+    @RequestMapping(value = "/error")
+    public Object error(HttpServletResponse resp, HttpServletRequest req) {
+        // 错误处理逻辑
+        return "其他异常";
     }
 }
