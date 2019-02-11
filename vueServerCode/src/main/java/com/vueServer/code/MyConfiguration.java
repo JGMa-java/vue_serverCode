713f3994d6c0118cd90f27fb28f67cf3f8ff8eaa
jgma <1520458127@qq.com>
2019-02-11 15:49:44 +0800

    CORS协议支持

---
+++ vueServerCode/src/main/java/com/vueServer/code/MyConfiguration.java
@@ -0,0 +1,24 @@
+package com.vueServer.code;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by MaJianGuo on 2019/02/11.下午 03:34
+ * cors跨域共享支持
+ */
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
