66555cfec2db25a18a196e409e2c76e2c11115a8
JGMa <18638691837@163.com>
2019-07-22 17:58:46 +0800

    mybatis逆向工程代码启动

---
+++ vueServerCode/src/main/java/com/vueServer/code/generator/StartUp.java
@@ -0,0 +1,16 @@
+//package com.vueServer.code.generator;
+//
+//import org.mybatis.generator.api.ShellRunner;
+//
+///**
+// * Created by Majg on 2019-07-15
+// **/
+//public class StartUp {
+//
+//
+//    public static void main(String[] args) {
+//        args = new String[] { "-configfile", "D:\\develop\\IDEA_workFolders\\hy_tool\\hy_dps\\src\\main\\resources\\mybatis-generator-config.xml", "-overwrite" };
+//        ShellRunner.main(args);
+//    }
+//
+//}
